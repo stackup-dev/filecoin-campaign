f83c0b5aff4f1d0dbd727fce5ed20d7ce9a3eac8
StackUp <110076027+stackup-dev@users.noreply.github.com>
2023-03-03 22:09:30 +0800

    Create interact.js

---
+++ scripts/interact.js
@@ -0,0 +1,18 @@
+const { ethers } = require("hardhat");
+
+async function main() {
+    const coinFile = await ethers.getContractFactory("CoinFile");
+    const contract = await coinFile.attach("YOUR_CONTRACT_ADDRESS");
+    const owner = await contract.owner();
+    let balance = await contract.balanceOf(owner);
+    // Now you can call functions of the contract
+    console.log(`The token balance is: ${balance}.`);
+
+}
+
+main()
+    .then(() => process.exit(0))
+    .catch((error) => {
+        console.error(error);
+        process.exit(1);
+    });
