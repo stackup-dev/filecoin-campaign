2e826d3f683143594daa357ecd63ee9f67c7240a
Ayan Masood <ayan.m.dev@gmail.com>
2023-03-28 17:33:08 +0530

    NFT Minting

--- scripts/mint-nft.mjs
+++ scripts/mint-nft.mjs
@@ -1,7 +1,13 @@
-const CONTRACT_ADDRESS = "PASTE_CONTRACT_ADDRESS";
-const META_DATA_URL = "PASTE_URL_HERE";
+const CONTRACT_ADDRESS = "0x920690ccfe4443b29F689e2447b5ccF0BEB0cbE9";
+const META_DATA_URL = "ipfs://bafyreiezby4doblwa2lmhzkezdfpit2iewvdhu4pqk3r6yth57fitthihm/metadata.json";
 
-// paste function here
+// Function to mint NFT
+async function mintNFT(contractAddress, metaDataURL) {
+  const ExampleNFT = await ethers.getContractFactory("ExampleNFT");
+  const [owner] = await ethers.getSigners();
+  await ExampleNFT.attach(contractAddress).mintNFT(owner.address, metaDataURL);
+  console.log("NFT minted to: ", owner.address);
+ }
 
 mintNFT(CONTRACT_ADDRESS, META_DATA_URL)
   .then(() => process.exit(0))
